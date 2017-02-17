01d9cc5f4981db5be659ff3342159fa99071e2aa
Георгий У <httpcatharsis@gmail.com>
2017-02-17 19:09:22 +0300

    Update exceptions.d

--- source/exceptions.d
+++ source/exceptions.d
@@ -4,21 +4,23 @@ class IOException : Exception {
     this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
         super(msg, file, line, next);
     }
+
+    this(string msg, Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
+        super(msg, file, line, next);
+    }
 }
 
 class NetworkException : Exception {
     this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
         super(msg, file, line, next);
     }
-}
 
-class ParseException : Exception {
-    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
+    this(string msg, Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
         super(msg, file, line, next);
     }
 }
 
-class EncodingException : Exception {
+class IllegalArgumentException : Exception {
     this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
         super(msg, file, line, next);
     }
