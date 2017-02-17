5bc1d3f6179540db41b31e48a4473fa299cd2569
George <httpcatharsis@gmail.com>
2017-02-17 16:54:13 +0300

    Init.

---
+++ source/exceptions.d
@@ -0,0 +1,25 @@
+module exceptions;
+
+class IOException : Exception {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
+        super(msg, file, line, next);
+    }
+}
+
+class NetworkException : Exception {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
+        super(msg, file, line, next);
+    }
+}
+
+class ParseException : Exception {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
+        super(msg, file, line, next);
+    }
+}
+
+class EncodingException : Exception {
+    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
+        super(msg, file, line, next);
+    }
+}
