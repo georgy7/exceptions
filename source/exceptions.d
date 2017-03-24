2391fb44737eb2791be0d6a011ba5ad536f321d6
Георгий Устинов <httpcatharsis@gmail.com>
2017-03-24 22:17:04 +0300

    Changed ddoc. Renamed all.

--- source/exceptions.d
+++ source/exceptions.d
@@ -1,71 +1,46 @@
 module exceptions;
 
-/++ For files, whichever type of streams, etc. +/
-class IOException : Exception {
-    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
-        super(msg, file, line, next);
-    }
+import std.exception;
 
-    this(string msg, Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super(msg, file, line, next);
-    }
-
-    this(Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super("", file, line, next);
-    }
-}
-
-class NetworkException : Exception {
-    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
-        super(msg, file, line, next);
-    }
-
-    this(string msg, Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super(msg, file, line, next);
-    }
-
-    this(Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super("", file, line, next);
-    }
+/++
+ + Abstract. If you want to catch simply all stream interruptions.
+ +/
+abstract class InterruptedStreamException : Exception {
+    mixin basicExceptionCtors;
 }
 
 /++
- + Applicable to wide range of situations, related to incorrect
- + user behaviour, including the validation failures of the user input.<br/>
- + It even can be used as the base class for all exceptions with messages,
- + that must be shown to user.
- + It is pretty common pattern to have such exception class.<br/>
- + Note, that in case of Internet applications, you should not show
- + user the stacktrace of these exceptions, but you should log it instead.<br/>
- + When your exception has the empty message, just show user a standard error notice.
- + Everything else will be in the logs.
+ + The input stream was not closed properly.
+ + Somebody is trying to read from it, but the stream can't give anything by some reason.
  +/
-class ArgumentException : Exception {
-    this(string msg, string file = __FILE__, size_t line = __LINE__, Throwable next = null) pure nothrow @nogc @safe {
-        super(msg, file, line, next);
-    }
-
-    this(string msg, Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super(msg, file, line, next);
-    }
-
-    this(Throwable next, string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super("", file, line, next);
-    }
+class InterruptedInputException : InterruptedStreamException {
+    mixin basicExceptionCtors;
 }
 
 /++
- + Good for prototyping.<br/>
- + For example, you have an interface. And you are going to implement it,
- + but you don't want to implement all these methods right now.<br/>
- + In this case, it's better to throw the exceptions from them,
- + than to return `null`, `-1`, the empty strings, etc.<br/>
- + And you also should not catch them.
- + Thus, the unimplemented things are always in sight.
+ + The output stream was not closed properly.
+ + Somebody is trying to write into it, but it can't receive the data by some reason.
  +/
-final class NotImplementedException : Exception {
+class InterruptedOutputException : InterruptedStreamException {
+    mixin basicExceptionCtors;
+}
+
+/++
+ + Abstract. Just a base to differ the user things from the system things.
+ +/
+abstract class UserBehaviourException : Exception {
+    mixin basicExceptionCtors;
+}
+
+/++
+ + If you have some good reason to not use `assert(0)`.<br/>
+ + In other words, if you really think you need a recoverable `assert(0)`.<br/>
+ + Be careful.
+ + See <a href="https://forum.dlang.org/post/mailman.1239.1490287548.31550.digitalmars-d@puremagic.com" target="_blank">the discussion</a>.
+ +/
+final class RecoverableHellException : Exception {
     /// Just use it without arguments.
     this(string file = __FILE__, size_t line = __LINE__) pure nothrow @nogc @safe {
-        super("Not implemented yet.", file, line);
+        super("Bloody hell.", file, line);
     }
 }
